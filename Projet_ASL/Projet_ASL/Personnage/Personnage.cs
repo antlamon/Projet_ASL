dad4796068d2f7e8f124620cea96115bcfbb93a2
Antoine Lamontagne <Antoine Lamontagne@DESKTOP-HO3P8ID>
2017-05-03 09:05:30 -0400

    vie

--- Projet_ASL/Projet_ASL/Personnage/Personnage.cs
+++ Projet_ASL/Projet_ASL/Personnage/Personnage.cs
@@ -8,7 +8,7 @@ namespace Projet_ASL
 {
     public abstract class Personnage : ObjetDeDémo
     {
-        public const int PTS_VIE_MAX = 100;
+        public const int PTS_VIE_MAX = 50;
         const int NB_TOURS_EN_FEU = 3;
         int ptsDeVie;
         public int PtsDeVie
